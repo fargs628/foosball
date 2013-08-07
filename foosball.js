6c6a00292131f034e8ca3ae3a24e60568d184a8b
Nick Martin <martin.nick@gmail.com>
2013-08-07 18:16:09 -0400

    A whole bunch of stuff for a score tracker.

--- foosball.js
+++ foosball.js
@@ -1,19 +1,26 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to foosball.";
-  };
+var scores = new Meteor.Collection('scores');
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+if (Meteor.isClient) {
+
+  $(document).keyup(function(e) {
+    if (e.keyCode == 87) {
+      var doc = scores.findOne({team:1});
+      scores.update({_id:doc._id},{$set:{score:doc.score + 1}});
+    } else if (e.keyCode == 73) {
+      var doc = scores.findOne({team:2});
+      scores.update({_id:doc._id},{$set:{score:doc.score + 1}});
     }
   });
-}
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Template.scores.helpers({
+    currentScore: function() {
+      if (scores.find().count() > 0) {
+        var scoreOne = scores.findOne({team:1}).score;
+        var scoreTwo = scores.findOne({team:2}).score;
+        return scoreOne + ' to ' + scoreTwo;
+      } else {
+        return 'Start a game!';
+      }
+    }
   });
 }
