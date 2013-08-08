3c63d88d04998506d34e37dccc80081125808334
Nick Martin <martin.nick@gmail.com>
2013-08-08 18:24:50 -0400

    Bunch of stuff to make this prettier, reset button.

--- foosball.js
+++ foosball.js
@@ -4,23 +4,41 @@ if (Meteor.isClient) {
 
   $(document).keyup(function(e) {
     if (e.keyCode == 87) {
-      var doc = scores.findOne({team:1});
-      scores.update({_id:doc._id},{$set:{score:doc.score + 1}});
+      incrementScore(1);
     } else if (e.keyCode == 73) {
-      var doc = scores.findOne({team:2});
-      scores.update({_id:doc._id},{$set:{score:doc.score + 1}});
+      incrementScore(2);
     }
   });
 
+  function incrementScore(team) {
+    var doc = scores.findOne({team:team});
+    scores.update({_id:doc._id},{$set:{score:doc.score + 1}});
+  }
+
+  function getScore(team) {
+    var score = scores.findOne({team:team});
+    return score ? score.score : 0;
+  }
+
   Template.scores.helpers({
     currentScore: function() {
-      if (scores.find().count() > 0) {
-        var scoreOne = scores.findOne({team:1}).score;
-        var scoreTwo = scores.findOne({team:2}).score;
-        return scoreOne + ' to ' + scoreTwo;
-      } else {
-        return 'Start a game!';
-      }
+        return getScore(1) + ' to ' + getScore(2);
+    }
+  });
+
+  Template.controls.events({
+    'click #start': function() {
+      Meteor.call('resetScores');
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.methods({
+    'resetScores': function() {
+      scores.remove({});
+      scores.insert({team:1, score:0});
+      scores.insert({team:2, score:0});
     }
   });
 }
